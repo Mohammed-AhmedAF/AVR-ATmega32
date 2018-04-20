fa94118b04444891a361a19cced9b4d34505fe54
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-04-20 23:42:11 +0200

    First special character project

---
+++ tests/specialCharacters/main.c
@@ -0,0 +1,26 @@
+#include "Std_Types.h"
+#include "Macros.h"
+#include "DIO_interface.h"
+#include "LCD_interface.h"
+
+u8 i = 0;
+int main(void) {
+	LCD_vidInit();
+
+	u8 b[8] = { 0x04,
+	  0x0E,
+	  0x15,
+	  0x04,
+	  0x04,
+	  0x15,
+	  0x0E,
+	  0x04};
+	LCD_vidSendCommand(0x40);
+	while(i != 8) {	
+		LCD_vidWriteCharacter(b[i]);	
+		i++;
+	} 
+	LCD_vidSendCommand(0b10000000);
+	LCD_vidWriteCharacter(0x00);
+	while(1);
+}
