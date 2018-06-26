d4d40d8f44d059dc66c5c1adce40658444683457
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-06-26 17:48:28 +0200

    External interrupt control is added

--- VariableBlinker/main.c
+++ VariableBlinker/main.c
@@ -5,6 +5,7 @@
 #include <avr/interrupt.h>
 
 u16 u16count = 0;
+u16 u16topCount = 0;
 void main(void) {
 
 	DIO_vidSetPinDirection(DIO_PORTB,DIO_PIN3,DIO_OUTPUT);
@@ -26,14 +27,31 @@ void main(void) {
 	/*Setting interrupt*/
 	SET_BIT(SREG,7);  /*Global interrupt enable*/
 	SET_BIT(TIMSK,0); /*Timer overflow enable*/
+
+	/*External interrupt*/
+	SET_BIT(GICR,6); //External interrupt request enable
+
+	/*Interrupt sense control*/
+	CLEAR_BIT(MCUCR,0);  /*Low level generates interrupt request*/
+	CLEAR_BIT(MCUCR,1); 
 	
 	while(1);
 }
 
 ISR(TIMER0_OVF_vect) {
 	u16count++;
-	if (u16count == 30000) {
+	if (u16count == u16topCount) {
 		TOGGLE_BIT(PORTB,DIO_PIN3);
 		u16count = 0;
+	}
+}
+
+//ISR for external interrupt 0;
+ISR(INT0_vect) {
+	if(u16topCount == 10000) {
+		u16topCount = 30000;
+	}
+	else {
+		u16topCount = 10000;
 	}
 }
