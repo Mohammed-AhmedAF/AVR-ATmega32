f277fc6166beb1387344ac3c10c1d531beccc305
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-09-22 22:16:27 +0200

    Separting blocks in init

--- DigitalClock/main.c
+++ DigitalClock/main.c
@@ -1,6 +1,6 @@
 /*
  * Author: Mohammed Ahmed Abd Al-Fattah
- * Project: Digital Clock
+ * Project: Digital Clock for AVR Atmega32 
  *
  */
 
@@ -10,7 +10,7 @@
 #include "LCD_interface.h"
 #include "TIMER0_interface.h"
 #include "INTERRUPTS_interface.h"
-
+#include <util/delay.h>
 
 #define SHIFT 4
 
@@ -20,9 +20,12 @@ volatile u8 min = 0;
 volatile u16 sec = 0;
 volatile u32 u32OVFCount = 0;
 volatile u8 i = 0;
+volatile u8 Flag = 0;
 
+/*Function prototypes*/
 void vidCount(void);
 void vidInitClock(void);
+void vidIncrementHour(void);
 
 void vidCount(void) {
 	u32OVFCount++;
@@ -58,9 +61,8 @@ void vidCount(void) {
 					/*Writing hours*/
 					LCD_vidGoToXY(0+SHIFT,0);
 					LCD_vidWriteNumber(hour/10);
-					LCD_vidGoToXY(0+SHIFT,1);
+					LCD_vidGoToXY(1+SHIFT,0);
 					LCD_vidWriteNumber(hour%10);
-
 				}
 				else {
 					hour = 0;
@@ -69,6 +71,54 @@ void vidCount(void) {
 		}
 	}
 
+}
+
+
+void main(void) {
+	/*Initialization*/
+	LCD_vidInit();
+	vidInitClock();
+	/*Setting External Interrupt pin direction as Input*/
+	DIO_vidSetPinDirection(DIO_PORTD,DIO_PIN2,DIO_INPUT);
+
+	TIMER0_vidInit(TIMER0_WGM_NORMAL,TIMER0_COM_NORMAL,TIMER0_CLK_1);
+
+	INTERRUPTS_vidSetGlobalInterruptFlag();
+
+	/*
+	   INTERRUPTS_vidSetInterruptEnable(INTERRUPTS_TOIE_0);
+	   INTERRUPTS_vidPutISRFunction(vidCount,INTERRUPTS_USUAL);
+	   */
+	INTERRUPTS_vidSetInterruptEnable(INTERRUPTS_INT_0); 
+	INTERRUPTS_vidPutISRFunction(vidIncrementHour,INTERRUPTS_INT_0);
+	INTERRUPTS_vidSetSenseControl(INTERRUPTS_INT_0,INTERRUPTS_SC_RISING);
+
+
+	while(1) {
+		Flag = 0;
+	}
+}
+
+void vidIncrementHour(void) {
+	INTERRUPTS_vidClearGlobalInterruptFlag();
+	while (GET_BIT(PORTD,DIO_PIN2) == 1);
+	if (Flag == 0) {
+		Flag = 1;
+		hour++;
+		if (hour < 23) {
+			LCD_vidGoToXY(0+SHIFT,0);	
+			LCD_vidWriteNumber(hour/10);
+			LCD_vidGoToXY(1+SHIFT,0);
+			LCD_vidWriteNumber(hour%10);
+		}
+		else {
+			hour = 0;
+			LCD_vidGoToXY(0+SHIFT,0);	
+			LCD_vidWriteNumber(hour/10);
+			LCD_vidGoToXY(1+SHIFT,0);
+			LCD_vidWriteNumber(hour%10);
+		}
+	}
 }
 
 void vidInitClock(void) {
@@ -84,13 +134,3 @@ void vidInitClock(void) {
 	LCD_vidWriteInPlace(7+SHIFT,'0');
 }
 
-void main(void) {
-	/*Initialization*/
-	LCD_vidInit();
-	vidInitClock();
-	TIMER0_vidInit(TIMER0_WGM_NORMAL,TIMER0_COM_NORMAL,TIMER0_CLK_1);
-	INTERRUPTS_vidSetInterruptEnable(INTERRUPTS_TOIE_0);
-	INTERRUPTS_vidSetGlobalInterruptFlag();
-	INTERRUPTS_vidPutISRFunction(vidCount);
-	while(1);
-}
