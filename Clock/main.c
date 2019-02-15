849befaa2412c2be5fc4dec3b2360e1043230d8c
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2019-02-15 14:57:21 +0200

    CLOCK: Adding comment to main.c explaining purpose

--- Clock/main.c
+++ Clock/main.c
@@ -1,7 +1,14 @@
+/*
+ * Author: Mohamed Ahmed Abd Al-Fattah
+ * Purpose; Clock with alarm
+ *
+ */
+
 #include "Std_Types.h"
 #include "Macros.h"
 #include "DIO_interface.h"
 #include "TIMER0_interface.h"
+#include "INTERRUPTS_private.h"
 #include "INTERRUPTS_interface.h"
 #include "LCD_interface.h"
 #include "CLOCK_interface.h"
@@ -11,8 +18,13 @@ void main(void) {
 	TIMER0_vidInit(TIMER0_COM_NORMAL,TIMER0_WGM_NORMAL,TIMER0_CLK_1);
 	INTERRUPTS_vidSetInterruptEnable(INTERRUPTS_TOIE_0);
 	INTERRUPTS_vidPutISRFunction(CLOCK_vidCount);
-	INTERRUPTS_vidSetGlobalInterruptFlag();
+	_Int_EN();
 	LCD_vidInit();
 
-	while(1);
+	LCD_vidGoToXY(LCD_XPOS0,1);
+	while(1) {
+		LCD_vidGoToXY(1,2);
+		LCD_vidWriteCharacter('1');
+		LCD_vidWriteCharacter('A');
+	}
 }
