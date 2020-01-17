504014443b700eba842fff462a02f3dddbbb545b
Mohammed Ahmed Abd Al-Fattah <mohamedstarwars@gmail.com>
2020-01-17 19:33:31 +0200

    Adding try/except to python code to catch serial exceptions

--- LightControl/gui.py
+++ LightControl/gui.py
@@ -4,10 +4,22 @@ from tkinter import *
 from functools import partial
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0',9600)
+try:
+    ser = serial.Serial('/dev/ttyUSB0',9600)
+except:
+    print("Error in establishing connection")
 
 def driveLED(ledNumber):
-   ser.write(str.encode(ledNumber))
+    try:
+        ser.write(str.encode(ledNumber))
+    except:
+        print("Error in sending byte")
+
+def driveLEDAgain():
+    try:
+        ser.write(str.encode(clicked.get()))
+    except:
+        print("Error in sending byte")
 
 root = Tk()
 
@@ -17,7 +29,7 @@ root.title("LED control")
 photo = PhotoImage(file="diode.png")
 root.iconphoto(False,photo)
 
-root.geometry("530x40")
+root.geometry("580x100")
 #Fixed window size
 root.resizable(width=False,height=False)
 
@@ -29,9 +41,16 @@ button4 = Button(root,text="LED 5",command=partial(driveLED,'e'))
 button5 = Button(root,text="LED 6",command=partial(driveLED,'f'))
 button6 = Button(root,text="LED 7",command=partial(driveLED,'g'))
 button7 = Button(root,text="LED 8",command=partial(driveLED,'h'))
+buttonA = Button(root,text="Send!",command=driveLEDAgain)
+labelOfTimer = Label(root,text="Timer: ",border=12)
+clicked = StringVar()
 
 
+option = OptionMenu(root,clicked,"a","b","c","d","e","f","g","h")
+clicked.set("a")
 
+
+#Placing widgets
 button0.grid(row=0,column=0,sticky=W,padx=2)
 button1.grid(row=0,column=1,sticky=W)
 button2.grid(row=0,column=2,sticky=W)
@@ -40,6 +59,9 @@ button4.grid(row=0,column=4,sticky=W)
 button5.grid(row=0,column=5,sticky=W)
 button6.grid(row=0,column=6,sticky=W)
 button7.grid(row=0,column=7,sticky=W,padx=2)
+buttonA.grid(row=1,column=2,sticky=W)
 
+labelOfTimer.grid(row=1,column=0,sticky=W)
+option.grid(row=1,column=1,sticky=W,padx=2)
 
 root.mainloop()
